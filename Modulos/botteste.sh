b8fc762c7f9eb642c861e30256ddf0ef81740583
Gatesccn <99981713+Gatesccn@users.noreply.github.com>
2022-07-12 09:40:46 -0400

    Update botteste.sh

--- Modulos/botteste.sh
+++ Modulos/botteste.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 [[ $(screen -list| grep -c 'bot_teste') == '0' ]] && {
     clear
-    echo -e "\E[44;1;37m     ATIVACÃO BOT SSH TESTE     \E[0m"
+    echo -e "\E[44;1;37m    ACTIVACION DEL BOT PRUEBAS     \E[0m"
     echo ""
-    echo -ne "\n\033[1;32mINFORME O TOKEN\033[1;37m: "
+    echo -ne "\n\033[1;32mAGREGRE SU TOKEN\033[1;37m: "
     read token
     clear
     echo ""
-    echo -e "\033[1;32mINICIANDO BOT TESTE \033[0m\n"
+    echo -e "\033[1;32mINICIANDO BOT DE DEMOS \033[0m\n"
     cd $HOME/BOT
     rm -rf $HOME/BOT/botssh
-    wget https://github.com/alfainternet/SSHPLUS/raw/main/Sistema/botssh >/dev/null 2>&1
+    wget https://raw.githubusercontent.com/Gatesccn/modmanda/main/Modulos/botssh >/dev/null 2>&1
     chmod 777 botssh
     echo ""
     sleep 1
     screen -dmS bot_teste ./botssh $token > /dev/null 2>&1
     clear
-    echo "BOT ATIVADO"
+    echo "BOT ACTIVADO"
     menu
 } || {
     screen -r -S "bot_teste" -X quit
     clear
-    echo "BOT DESATIVADO"
+    echo "BOT DESACTIVADO"
     menu
 }
